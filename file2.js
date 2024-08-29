ac061a8d19b57fd33e32b161631860d9cd71870f
YsfUlt <ultras.01ysf@gmail.com>
2024-08-29 15:41:56 +0100

    hi git

--- file2.js
+++ file2.js
@@ -2,7 +2,7 @@ console.log("ysf change 1  ysfbranch ");
 console.log("said change 1  ysfbranch ");
 console.log("said change 2  saidbranch  ");
 
-console.log("push apres suppresion de dosiiser git")
+console.log("push apres suppresion de dosiiser git t")
 
 console.log("said new branch")
 console.log("said new branch")
