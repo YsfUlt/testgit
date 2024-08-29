345cc714fa63a510d903ed37b61560eaaa643a86
YsfUlt <ultras.01ysf@gmail.com>
2024-08-29 15:21:46 +0100

    apres sup dossier git

---
+++ file2.js
@@ -0,0 +1,8 @@
+console.log("ysf change 1  ysfbranch ");
+console.log("said change 1  ysfbranch ");
+console.log("said change 2  saidbranch  ");
+
+console.log("push apres supression dossier git")
+
+
+console.log("said new brancg")
---
+++ fileYsf.js
@@ -0,0 +1,15 @@
+console.log("hi file prmeir cree");
+console.log("change ysf 2  cree");
+
+console.log("change ysf 3  cree");
+
+console.log("said. new branch");
+
+
+
+const C = 1;
+const B = 2;
+
+console.log(A + B);
+console.log("said change 2");
+console.log("said change 3")
